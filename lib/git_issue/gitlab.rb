80288dd50a27f35afdec3ede18053b7e14fad708
shiftky <shiftky@gmail.com>
2014-11-01 01:36:56 +0900

    Show project's issues list

--- lib/git_issue/gitlab.rb
+++ lib/git_issue/gitlab.rb
@@ -17,14 +17,35 @@ module GitIssue
       configure_error('token', "git config issue.token XXXXXXXXXXXXXXXXXXXX")  if @token.blank?
     end
 
-    def commands
-      cl = super
-    end
-
     def list(options = {})
+      query_names = %i(state milestone labels)
+      params = query_names.inject({}) { |hash, key| hash[key] = options[key] if options[key]; hash }
+      params[:state] ||= 'open'
+
       url = to_url("projects", @repo.gsub("/", "%2F"), "issues")
-      issues = fetch_json(url)
-      p issues
+      issues = fetch_json(url, options, params)
+
+      issues = issues.sort_by { |issue| issue['iid'] }
+
+      title_max = issues.map { |issue| mlength(issue['title']) }.max
+      label_max = issues.map { |issue| mlength(issue['labels'].join(",")) }.max
+      username_max = issues.map { |issue| mlength(issue['author']['username']) }.max
+
+      issues.each do |issue|
+        comments_url = to_url("projects", @repo.gsub("/", "%2F"), "issues", issue['id'], "notes")
+        comments = fetch_json(comments_url)
+
+        puts sprintf('%s %s %s %s %s c:%s %s %s',
+                      apply_fmt_colors(:id, sprintf('#%-4d', issue['iid'])),
+                      apply_fmt_colors(:state, issue['state']),
+                      mljust(issue['title'], title_max),
+                      apply_fmt_colors(:labels, mljust(issue['labels'].join(","), label_max)),
+                      apply_fmt_colors(:author, mljust(issue['author']['username'], username_max)),
+                      comments.count,
+                      to_date(issue['created_at']),
+                      to_date(issue['updated_at'])
+        )
+      end
     end
 
     private
@@ -34,10 +55,16 @@ module GitIssue
     end
 
     def fetch_json(url, options = {}, params = {})
-      response = send_request(url, {}, {}, {}, :get)
+      response = send_request(url, {}, options, params, :get)
       json = JSON.parse(response.body)
       raise error_message(json) unless response_success?(response)
       json
+    end
+
+    def error_message(json)
+      msg = [json['message']]
+      msg += json['errors'].map(&:pretty_inspect) if json['errors']
+      msg.join("\n  ")
     end
 
     def send_request(url, json = {}, options = {}, params = {}, method)
@@ -60,16 +87,20 @@ module GitIssue
         request.body = json.to_json if json.present?
 
         response = http.request(request)
-
         response
       }
     end
 
-    def error_message(json)
-      msg = [json['message']]
-      msg += json['errors'].map(&:pretty_inspect) if json['errors']
-      msg.join("\n  ")
+    def apply_fmt_colors(key, str)
+      fmt_colors[key.to_sym] ? apply_colors(str, *Array(fmt_colors[key.to_sym])) : str
     end
 
+    def fmt_colors
+      @fmt_colors ||= { id: [:bold, :cyan],
+        state: :blue,
+        author: :magenta,
+        labels: :yellow,
+      }
+    end
   end
 end
